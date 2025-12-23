6b0757a74caff4e425b6a10a3620b199725e844c
Tobias Markus <tobbi.bugs@googlemail.com>
2025-12-23 18:44:37 +0100

    Add configurable raqm-config file

--- ports/libraqm/portfile.cmake
+++ ports/libraqm/portfile.cmake
@@ -14,4 +14,7 @@ vcpkg_install_meson()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
+configure_file("${CMAKE_CURRENT_LIST_DIR}/raqm-config.cmake.in"
+               "${CURRENT_PACKAGES_DIR}/share/${PORT}/raqm-config.cmake" @ONLY)
+
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
